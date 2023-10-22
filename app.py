cf13cfa69e74b34f89b7f6d0dfa9b65edd0cd60f
kprochacka <klaudiaprochacka@gmail.com>
2023-10-22 10:31:02 +0200

    pliki

--- app.py
+++ app.py
@@ -1,6 +1,46 @@
-f = open("data.txt", "r")
-for line in f:
-    print(line, end="")
-f.close()
+# f = open("data.txt", "r")
+# for line in f:
+#     print(line, end="")
+# f.close()
 
+
+# def file_lines(filepath):
+#     with open(filepath) as file:
+#         for line in file:
+#             yield line
+#
+#
+# for line in file_lines("data.txt"):
+#     print(line)
+
+
+
+# file = open("data.txt")
+#
+# while result := file.readline():
+#     print(result)
+#
+# file.close()
+
+
+
+# file = open("data.txt", "w")
+#
+# result = file.read(13)
+# print(result)
+#
+# file.close()
+
+# with open("hello.txt", "w") as file:
+#     for _ in range(10):
+#         file.write("Hello Marysia, przestan strzelac\n")
+
+
+# with open("hello.txt", "w") as file:
+#     file.writelines("Hello Marysia")
+
+
+# with open("hello.txt", "w") as file:
+#     for _ in range(10):
+#         print("Hello",  "world", sep=", ", file=file)
 
